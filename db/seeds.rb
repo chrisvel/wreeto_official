7d8c1a999b0620fbb9b66e0a77a745040d21b810
Chris Veleris <chrisveleris@gmail.com>
2020-05-10 18:44:50 +0300

    Add stdout message about default user creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
 # Basic test stuff
+puts "=> Creating default user [user@email.com:password]"
 User.create!({firstname: 'John', lastname: "Johnnson", email: 'user@email.com', password: 'password', confirmed_at: DateTime.now})
