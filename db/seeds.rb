f0eb136abae0451b42219eeeff9a108fb1fdf0af
Chris Veleris <chrisveleris@gmail.com>
2020-05-10 15:07:36 +0300

    Fix an issue with category ducplicate creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,2 @@
 # Basic test stuff
-User.create({firstname: 'John', lastname: "Johnnson", email: 'user@email.com', password: 'password', confirmed_at: DateTime.now})
-user = User.first
-user.categories.create({title: 'Projects', deletable: false})
+User.create!({firstname: 'John', lastname: "Johnnson", email: 'user@email.com', password: 'password', confirmed_at: DateTime.now})
