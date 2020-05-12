870fc027c55989d2e426b00691f2c11c4fc4386d
Chris Veleris <chrisveleris@gmail.com>
2020-05-12 22:52:25 +0300

    Fix an issue with seeds loading

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
 
-if [ -f /app/tmp/pids/server.pid ]; then
-  rm /app/tmp/pids/server.pid
+if [ -f /app/wreeto/tmp/pids/server.pid ]; then
+  rm /app/wreeto/tmp/pids/server.pid
 fi
 
 bundle exec bin/rails db:environment:set RAILS_ENV=development
 bundle exec rake db:migrate || bundle exec rake db:setup
 
-bundle exec foreman start 
+exec bundle exec "$@"
