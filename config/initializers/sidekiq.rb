48139da80fa791f43f8201bfdebcebd9f84a9d65
Chris Veleris <chrisveleris@gmail.com>
2020-05-14 15:37:30 +0300

    Setup REDIS_PASSWORD and POSTGRES_PORT env vars

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,19 +1,19 @@
 require 'sidekiq'
 require 'sidekiq-status'
 
+REDIS_CONFIG = {
+  host: ENV['REDIS_HOST'],
+  port: ENV['REDIS_PORT'] || '6379'
+}
+
+REDIS_CONFIG.merge!({password: ENV['REDIS_PASSWORD']}) unless ENV['REDIS_PASSWORD'].blank?
+
 Sidekiq.configure_server do |config|
-  config.redis = {
-    host: ENV['REDIS_HOST'],
-    port: ENV['REDIS_PORT'] || '6379'
-  }
-  Sidekiq::Status.configure_client_middleware config, expiration: 30.minutes
+  config.redis = REDIS_CONFIG
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = {
-    host: ENV['REDIS_HOST'],
-    port: ENV['REDIS_PORT'] || '6379'
-  }
+  config.redis = REDIS_CONFIG
   Sidekiq::Status.configure_server_middleware config, expiration: 30.minutes
   Sidekiq::Status.configure_client_middleware config, expiration: 30.minutes
 end
