ed6898cecf9c24292ebcfdd72fef1560b0c0edb5
Chris Veleris <chrisveleris@gmail.com>
2020-05-20 13:45:33 +0300

    Fix an issue with selected tags

--- app/helpers/inventory/notes_helper.rb
+++ app/helpers/inventory/notes_helper.rb
@@ -1,6 +1,6 @@
 module Inventory::NotesHelper
   def tag_links(note)
-    tags_formatted = note.tags.map do |tag| 
+    tags_formatted = note.tags.order(:name).map do |tag| 
       link_to("##{tag.name}", tag_path(tag: tag), class: 'badge badge-dark text-white') 
     end
     tags_formatted.join(" ").html_safe
