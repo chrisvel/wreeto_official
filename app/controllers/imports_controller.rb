2882eebefa8d8760f4d521d7bf25d118718107ea
Chris Veleris <chrisveleris@gmail.com>
2020-05-26 18:14:41 +0300

    Setup wizard form

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -2,12 +2,17 @@ class ImportsController < ApplicationController
   before_action :authenticate_user!
 
   def wizard
-    
+
   end
 
   def import_zip
+    byebug
     exporter = Downloads::ZipExporterJob.perform_now(current_user.id)
     send_file(exporter.zip_path, type: 'application/zip', disposition: 'attachment')
     Downloads::DeleteExportedZipJob.set(wait: 20.seconds).perform_later(exporter.fullpath)
+  end
+
+  def upload_params
+    params.require('zip_file')
   end
 end
