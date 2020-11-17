0518a04bd2f52b3ef026d152052d4d5834f7e2e5
Chris Veleris <chrisveleris@gmail.com>
2020-11-17 18:13:17 +0200

    Setup dev tools and fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 puts "=> Creating default user [admin:password]"
-account = Account.create!
-User.create!({username: 'admin', email: 'user@email.com', password: 'password', confirmed_at: DateTime.now, account: account})
+user = User.create!({username: 'admin', email: 'user@email.com', password: 'password', confirmed_at: DateTime.now})
+account = Account.create!(user: user)
