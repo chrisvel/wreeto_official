63cf8bf948a343836f38f9d0b8d09741f385edec
Chris Veleris <chrisveleris@gmail.com>
2020-11-23 16:03:31 +0200

    Fix an issue with migrations and new accounts

--- db/migrate/20200812134437_backfill_account.rb
+++ db/migrate/20200812134437_backfill_account.rb
@@ -1,6 +1,8 @@
 class BackfillAccount < ActiveRecord::Migration[5.2]
   def change
-    account = Account.first_or_create!
-    User.update_all(account_id: account.id)
+    if User.any?
+      account = Account.first_or_create!
+      User.update_all(account_id: account.id)
+    end
   end
 end
